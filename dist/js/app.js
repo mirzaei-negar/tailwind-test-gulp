abee8b65352b8c997ecf7807639a55d307810a53
Negar Mirzaei <negarm85@yahoo.com>
2020-08-19 11:49:23 +0430

    update

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,17 +1,18 @@
 const navSlide = () => {
-const button = document.querySelector(".more");
-const menu = document.querySelector(".side-menu");
-const cardHolder = document.querySelector(".card-holder");
-const cards = document.querySelectorAll(".card");
-const adminsBox = document.querySelector(".admins-box");
-button.addEventListener("click", () => {
-  menu.classList.toggle("active");
-  cardHolder.classList.toggle("margin");
-  adminsBox.classList.toggle("admin-margin");
-  button.classList.toggle("more-margin");
-  cards.forEach( (card) =>{
-    card.classList.add(".cards-width");
+  const button = document.querySelector(".more");
+  const menu = document.querySelector(".side-menu");
+  const cardHolder = document.querySelector(".card-holder");
+  const cards = document.querySelectorAll(".card");
+  const adminsBox = document.querySelector(".admins-box");
+  button.addEventListener("click", () => {
+    menu.classList.toggle("active");
+    cardHolder.classList.toggle("margin");
+    adminsBox.classList.toggle("admin-margin");
+    button.classList.toggle("more-margin");
+    cards.forEach((card) => {
+      card.classList.toggle("cards-width");
+    });
+    // cards.classList.toggle("cards-width");
   });
-});
 };
 navSlide();
